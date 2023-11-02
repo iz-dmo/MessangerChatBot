e3bb03bd0f034599a736ce19fabce4cc6dfba249
iz-dmo <mtk22718@gmail.com>
2023-11-02 09:07:52 +0630

    SetUP

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ChatBot</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+        </style>
+    </head>
+    <body>
+    </body>
+            
+            
+    <!-- script -->
+    <script>
+     var botmanWidget = {
+         aboutText: 'iz_dmo',
+         introMessage: " Hi I'm ChatBot"
+     };
+    </script>
+    <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
+      
+</html>
